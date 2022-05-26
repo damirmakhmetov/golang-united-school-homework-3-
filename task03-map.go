cf1f61d7b9bb9ad67fee5dd0fc1b058c77c43f80
Damir Makhmetov <damirmakhmetov@gmail.com>
2022-05-26 07:05:42 +0600

    solution 2

--- task03-map.go
+++ task03-map.go
@@ -4,11 +4,13 @@ import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	keys := make([]string, 0, len(input))
-	for _, k := range input {
+	keys := make([]int, 0, len(input))
+	for k := range input {
 		keys = append(keys, k)
 	}
-	sort.Strings(keys)
-	result = keys
+	sort.Ints(keys)
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
 	return result
 }
